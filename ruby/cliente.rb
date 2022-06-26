64823e1f7549f21beff54e4910107afdb5ef318b
DamiDelfino <damianedelfino.95@gmail.com>
2022-06-26 18:15:19 -0300

    Detallamos funciones

--- ruby/cliente.rb
+++ ruby/cliente.rb
@@ -3,16 +3,21 @@ require 'socket'
 hostname = 'localhost' #Indicamos el host
 port = 2000            #Indicamos el puerto
 
-coneccion= TCPSocket.open(hostname, port)
+conexion= TCPSocket.open(hostname, port) #Abrimos la conexion
 
+print "Puede empezar a escribir: "
 
-loop do
+loop do                 #Comenzamos el loop
     mensaje = gets
-    coneccion.puts mensaje
-    mensaje_servidor = coneccion.gets
-    puts mensaje_servidor
+    conexion.puts "Cliente dijo: " + mensaje  #Enviamos el mensaje al servidor
+    mensaje_servidor = conexion.gets          #Obtenemos el mensaje del servidor
+    puts  mensaje_servidor                    #Mostramos el mensaje en consola
+    print "Escribir: "
 end
 
 
+#Para correr el programa:
+#Correr el archivo servidor.rb primero
+#ruby cliente.rb
 
 
--- ruby/servidor.rb
+++ ruby/servidor.rb
@@ -1,24 +1,28 @@
 require 'socket'
 
-server = TCPServer.open(2000)
+server = TCPServer.open(2000)                       #Obtenemos el puerto del servidor
 
 puts "Servidor conectado en el puerto 2000..."
 loop {
-    Thread.start(server.accept) do |client|
+    Thread.start(server.accept) do |client|         #Conectamos el servidor
         
-        mensaje_cliente = client.gets
-        puts mensaje_cliente
+        mensaje_cliente = client.gets               #Obtenemos el mensaje del cliente
+        puts mensaje_cliente                        #Mostramos el mensaje en consola
         
         print "Puede empezar a escribir: "
         var = 0
-        while var < 10
+        while var < 10                              #Creamos el loop infinito
             enviar = gets.chomp
-            client.puts "servidor dijo :" + enviar
-            mensaje_cliente = client.gets
-            puts mensaje_cliente
+            client.puts "Servidor dijo :" + enviar  #Enviamos el mensaje al cliente
+            mensaje_cliente = client.gets           #Obtenemos el mensaje del cliente
+            puts mensaje_cliente                    #Mostramos el mensaje del cliente
+            print "Escribir: "
 
         end      
       
     end
 }
 
+
+#Para correr el archivo
+#ruby servidor.rb
